24bb5bb5203eb260480d99a3121aaa93764b11ca
voliveiraa236-lang <voliveiraa236@gmail.com>
2026-04-16 21:58:48 -0300

    update

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
+document.addEventListener("DOMContentLoaded", function(){
 
+    const menu = document.querySelector(".menu");
+    const openBtn = document.querySelector(".menu-btn");
+    const closeBtn = document.querySelector(".close-btn");
+    const overlay = document.querySelector(".overlay");
+
+    const searchBtn = document.querySelector(".search-btn");
+    const searchBox = document.querySelector(".search-box");
+
+    const cart = document.querySelector(".cart");
+    const cartCount = document.querySelector(".cart-count");
+
+    let count = 0;
+
+    // MENU
+    openBtn.onclick = () => {
+        menu.classList.add("active");
+        overlay.classList.add("active");
+    };
+
+    closeBtn.onclick = () => {
+        menu.classList.remove("active");
+        overlay.classList.remove("active");
+    };
+
+    overlay.onclick = () => {
+        menu.classList.remove("active");
+        overlay.classList.remove("active");
+    };
+
+    // PESQUISA
+    searchBtn.onclick = () => {
+        searchBox.style.display =
+            searchBox.style.display === "block" ? "none" : "block";
+    };
+
+    // CARRINHO
+    cart.onclick = () => {
+        count++;
+        cartCount.textContent = count;
+    };
+
+});
